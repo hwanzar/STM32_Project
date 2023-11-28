999dbffb9e53c1e419c5b8d883670da61b1602b7
BAOPHAN <phangiabao9999@gmail.com>
2023-11-28 11:20:53 +0700

    Add button.c

--- STM32F103RB/Core/Src/button.c
+++ STM32F103RB/Core/Src/button.c
@@ -4,5 +4,54 @@
  *  Created on: Nov 28, 2023
  *      Author: Lenovo
  */
+#include "button.h"
+
+uint16_t mode[4] = { BTN0_Pin, BTN1_Pin, BTN2_Pin, BTN3_Pin};
+
+int KeyReg0[4] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
+int KeyReg1[4] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
+int KeyReg2[4] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
+int KeyReg3[4] = {NORMAL_STATE, NORMAL_STATE, NORMAL_STATE, NORMAL_STATE};
+
+int TimeOutForKeyPress[4] = { 200, 200, 200, 200 };
+int button_flag[4] = {0,0,0,0};
 
 
+int isButtonPressed(int num){
+	if(button_flag[num] == 1){
+		button_flag[num] = 0;
+		return 1;
+	}
+	return 0;
+}
+void subKeyProcess(int num){
+	//TODO
+	button_flag[num]=1;
+}
+
+
+void getKeyInput(){
+	for (int i = 0; i < 4; i++){
+		KeyReg2[i] = KeyReg1[i];
+		KeyReg1[i] = KeyReg0[i];
+		//Add your button here
+		KeyReg0[i] = HAL_GPIO_ReadPin(GPIOB, mode[i]);
+
+		if ((KeyReg1[i] == KeyReg0[i]) && (KeyReg1[i] == KeyReg2[i])){
+			if (KeyReg2[i] != KeyReg3[i]){
+				KeyReg3[i] = KeyReg2[i];
+				if (KeyReg3[i] == PRESSED_STATE){
+					TimeOutForKeyPress[i] = 200;
+					subKeyProcess(i);
+				}
+			}
+
+			else{ //press without release
+				TimeOutForKeyPress[i]--;
+				if (TimeOutForKeyPress[i] == 0){
+					KeyReg3[i] = NORMAL_STATE;
+				}
+			}
+		}
+	}
+}
