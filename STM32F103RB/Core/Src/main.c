2a1b487e5f658d7466b476453058de2c7c27a4e3
amopdz <117839843+amopdz@users.noreply.github.com>
2023-11-29 15:36:37 +0700

    added scheduler into main.c

--- STM32F103RB/Core/Src/main.c
+++ STM32F103RB/Core/Src/main.c
@@ -28,6 +28,7 @@
 #include "software_timer.h"
 #include "led_control.h"
 #include "button.h"
+#include "scheduler.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -47,6 +48,8 @@
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim2;
 
+UART_HandleTypeDef huart2;
+
 /* USER CODE BEGIN PV */
 
 /* USER CODE END PV */
@@ -55,6 +58,7 @@ TIM_HandleTypeDef htim2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_USART2_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -102,14 +106,16 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
+  SCH_Add_Task(&fsm_auto, 20, 50);
+  SCH_Add_Task(&fsm_pedestrian, 30, 50);
+  char str[40];
+  //SCH_Add_Task(&fsm_auto, 50, 50);
   while (1)
   {
-	  HAL_UART_Transmit(&huart2, (void*)str, sprintf(str, "Dispatching task\r\n"), 200);
-	  fsm_auto();
+	  //HAL_UART_Transmit(&huart2, (void*)str, sprintf(str, "Dispatching task\r\n"), 200);
+	  SCH_Dispatch_Tasks();
 //	  fsm_manual();
 //	  fsm_tuning();
-	  fsm_pedestrian();
 
     /* USER CODE END WHILE */
 
@@ -201,6 +207,39 @@ static void MX_TIM2_Init(void)
 }
 
 /**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
   * @brief GPIO Initialization Function
   * @param None
   * @retval None
@@ -249,6 +288,7 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
+	SCH_Update();
 	timerRun();
 	getKeyInput();
 }
